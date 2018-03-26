ed96659e8341eab9ffc0330827a80c60fb4b8281
user2 <oritgurel@gmail.com>
2018-03-26 12:51:30 +0300

    create_check_change_set_withBug

--- app/build/generated/source/r/debug/android/support/mediacompat/R.java
+++ app/build/generated/source/r/debug/android/support/mediacompat/R.java
@@ -26,9 +26,9 @@ public final class R {
         public static final int notification_icon_bg_color = 0x7f050044;
         public static final int notification_material_background_media_default_color = 0x7f050045;
         public static final int primary_text_default_material_dark = 0x7f05004a;
-        public static final int ripple_material_light = 0x7f05004f;
-        public static final int secondary_text_default_material_dark = 0x7f050050;
-        public static final int secondary_text_default_material_light = 0x7f050051;
+        public static final int ripple_material_light = 0x7f050050;
+        public static final int secondary_text_default_material_dark = 0x7f050051;
+        public static final int secondary_text_default_material_light = 0x7f050052;
     }
     public static final class dimen {
         public static final int compat_button_inset_horizontal_material = 0x7f06004d;
@@ -77,50 +77,50 @@ public final class R {
         public static final int blocking = 0x7f080024;
         public static final int cancel_action = 0x7f080027;
         public static final int chronometer = 0x7f08002a;
-        public static final int end_padder = 0x7f080036;
-        public static final int forever = 0x7f08003b;
-        public static final int icon = 0x7f080040;
-        public static final int icon_group = 0x7f080041;
-        public static final int info = 0x7f080045;
-        public static final int italic = 0x7f080048;
-        public static final int line1 = 0x7f080049;
-        public static final int line3 = 0x7f08004a;
-        public static final int media_actions = 0x7f080050;
-        public static final int normal = 0x7f080057;
-        public static final int notification_background = 0x7f080058;
-        public static final int notification_main_column = 0x7f080059;
-        public static final int notification_main_column_container = 0x7f08005a;
-        public static final int right_icon = 0x7f08006a;
-        public static final int right_side = 0x7f08006b;
-        public static final int status_bar_latest_event_content = 0x7f08008c;
-        public static final int text = 0x7f080091;
-        public static final int text2 = 0x7f080092;
-        public static final int time = 0x7f080095;
-        public static final int title = 0x7f080096;
+        public static final int end_padder = 0x7f080039;
+        public static final int forever = 0x7f08003d;
+        public static final int icon = 0x7f080042;
+        public static final int icon_group = 0x7f080043;
+        public static final int info = 0x7f080047;
+        public static final int italic = 0x7f08004a;
+        public static final int line1 = 0x7f08004b;
+        public static final int line3 = 0x7f08004c;
+        public static final int media_actions = 0x7f080052;
+        public static final int normal = 0x7f080059;
+        public static final int notification_background = 0x7f08005a;
+        public static final int notification_main_column = 0x7f08005b;
+        public static final int notification_main_column_container = 0x7f08005c;
+        public static final int right_icon = 0x7f08006c;
+        public static final int right_side = 0x7f08006d;
+        public static final int status_bar_latest_event_content = 0x7f08008e;
+        public static final int text = 0x7f080093;
+        public static final int text2 = 0x7f080094;
+        public static final int time = 0x7f080097;
+        public static final int title = 0x7f080098;
     }
     public static final class integer {
         public static final int cancel_button_image_alpha = 0x7f090002;
         public static final int status_bar_notification_info_maxnum = 0x7f090004;
     }
     public static final class layout {
-        public static final int notification_action = 0x7f0a0021;
-        public static final int notification_action_tombstone = 0x7f0a0022;
-        public static final int notification_media_action = 0x7f0a0023;
-        public static final int notification_media_cancel_action = 0x7f0a0024;
-        public static final int notification_template_big_media = 0x7f0a0025;
-        public static final int notification_template_big_media_custom = 0x7f0a0026;
-        public static final int notification_template_big_media_narrow = 0x7f0a0027;
-        public static final int notification_template_big_media_narrow_custom = 0x7f0a0028;
-        public static final int notification_template_custom_big = 0x7f0a0029;
-        public static final int notification_template_icon_group = 0x7f0a002a;
-        public static final int notification_template_lines_media = 0x7f0a002b;
-        public static final int notification_template_media = 0x7f0a002c;
-        public static final int notification_template_media_custom = 0x7f0a002d;
-        public static final int notification_template_part_chronometer = 0x7f0a002e;
-        public static final int notification_template_part_time = 0x7f0a002f;
+        public static final int notification_action = 0x7f0a0022;
+        public static final int notification_action_tombstone = 0x7f0a0023;
+        public static final int notification_media_action = 0x7f0a0024;
+        public static final int notification_media_cancel_action = 0x7f0a0025;
+        public static final int notification_template_big_media = 0x7f0a0026;
+        public static final int notification_template_big_media_custom = 0x7f0a0027;
+        public static final int notification_template_big_media_narrow = 0x7f0a0028;
+        public static final int notification_template_big_media_narrow_custom = 0x7f0a0029;
+        public static final int notification_template_custom_big = 0x7f0a002a;
+        public static final int notification_template_icon_group = 0x7f0a002b;
+        public static final int notification_template_lines_media = 0x7f0a002c;
+        public static final int notification_template_media = 0x7f0a002d;
+        public static final int notification_template_media_custom = 0x7f0a002e;
+        public static final int notification_template_part_chronometer = 0x7f0a002f;
+        public static final int notification_template_part_time = 0x7f0a0030;
     }
     public static final class string {
-        public static final int status_bar_notification_info_overflow = 0x7f0c0023;
+        public static final int status_bar_notification_info_overflow = 0x7f0c0027;
     }
     public static final class style {
         public static final int TextAppearance_Compat_Notification = 0x7f0d0100;
--- app/src/main/java/com/oritmalki/quizapp/view/CreateQuizFragment.java
+++ app/src/main/java/com/oritmalki/quizapp/view/CreateQuizFragment.java
@@ -2,15 +2,18 @@ package com.oritmalki.quizapp.view;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.os.Build;
 import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
-import android.view.Gravity;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,31 +21,31 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.RadioGroup.LayoutParams;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.oritmalki.quizapp.Data.QuestionsRepository;
 import com.oritmalki.quizapp.R;
 import com.oritmalki.quizapp.model.Answer;
 import com.oritmalki.quizapp.model.Question;
+import com.oritmalki.quizapp.model.Quiz;
 import com.oritmalki.quizapp.view.QuestionFragment.QuestionFragmentListener;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * Created by Orit on 1.2.2018.
  */
 
-public class CreateQuizFragment extends Fragment implements View.OnClickListener, OnItemSelectedListener {
+public class CreateQuizFragment extends Fragment implements View.OnClickListener, OnItemSelectedListener, CompoundButton.OnCheckedChangeListener {
 
     //declare layout views...
     private static final String ARGS_QUESTION_ID = "args_question_id";
@@ -67,10 +70,17 @@ public class CreateQuizFragment extends Fragment implements View.OnClickListener
     private Button removeAnswerButt;
     private Button addAnswerButt;
     private ViewGroup answerButtons;
+    private ViewGroup controlTitles;
+    private TextView isCorrectTv;
     int questionType;
     private LayoutInflater inflater;
+    private Quiz quiz;
+    private String quizName;
+    private Switch[] isCorrect;
 
     List<Question> questionList = new ArrayList<>();
+    Answer[] answers;
+
 
 
 
@@ -98,6 +108,8 @@ public class CreateQuizFragment extends Fragment implements View.OnClickListener
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
     }
 
 
@@ -106,7 +118,12 @@ public class CreateQuizFragment extends Fragment implements View.OnClickListener
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
+        //TODO add a dialogue for setting quiz name (only on first fragment)
+        quiz = new Quiz(quizName, questionList);
+
         View view = inflater.inflate(R.layout.create_quiz_fragment_layout, container, false);
+
+        setQuizNameDialog();
 
         initializeViews(view);
 
@@ -134,39 +151,63 @@ public class CreateQuizFragment extends Fragment implements View.OnClickListener
 
             case R.id.save_question:
                 //save question
-                if (questionET.getText() != null && questionET.getText().length() > 0) {
+                Question question;
+                String questionText = "";
+                String answer;
+                int type;
+                int answersCount=0;
+                Answer[] answers = new Answer[answersCount];
 
-//                    questionList.add(new Question(getArguments().getInt(ARGS_QUESTION_ID, questionET.getText().toString(),
-//                            getQuestionType(typeSpinner.toString(),))));
+                //get question text
+                if (questionET.getText() != null && questionET.getText().length() > 0) {
+                    questionText = questionET.getText().toString();
+                } else {
+
+                    //TODO popup dialogue show message "Please state question text."
                 }
+                    for (int i=1; i<createQuestionLayout.getChildCount(); i++) {
+                        answersCount=0;
+                        if (createQuestionLayout.getChildAt(i) instanceof EditText) {
+                            EditText et = (EditText) createQuestionLayout.getChildAt(i);
+                            answersCount++;
+
+                            //TODO add boolean input for correct/incorrect
+
+//                            if (et.getText() != null && et.getText().length() > 0) {
+//                                answer = et.getText().toString();
+//                                answers[i] = answer;
+//                            }
+//                            else {
+//                                //TODO popup dialogue show message "You haven't filled in all answer fields"
+//                            }
+                        }
+
+                    }
+                    //TODO questionID = viewPager.get fragment current index...
+                    question = new Question(0, questionText, getQuestionType(typeSpinner.getSelectedItem().toString()), answers);
+                    questionList.add(question);
+
+
+                    //TODO save and go to next page
+
+                //TODO add another button for finish - that will lead to the list of quizes with the new one updated (add the quiz to the quiz list and go to list fragment)
+
+//
 
                 break;
 
             case R.id.add_answer_butt:
                 //add another answer
 
-                EditText answer = (EditText) inflater.inflate(R.layout.answer_et, createQuestionLayout, false);
-                answer.setId(View.generateViewId());
-                RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams) answer.getLayoutParams();
-                if (createQuestionLayout.getChildCount() > 0) {
-
-                    if (addAnswerLayout == createQuestionLayout.getChildAt(0) && addAnswerLayout == createQuestionLayout.getChildAt(createQuestionLayout.getChildCount()-1)) {
-                        lp.addRule(RelativeLayout.BELOW, addAnswerLayout.getId());
-                    } else
-                    lp.addRule(RelativeLayout.BELOW, createQuestionLayout.getChildAt(createQuestionLayout.getChildCount()-1).getId());
-                }
-                answer.setLayoutParams(lp);
-                createQuestionLayout.addView(answer);
-                answer.setHint("Insert answer " + (createQuestionLayout.indexOfChild(createQuestionLayout.getChildAt(createQuestionLayout.getChildCount()-1))+1));
+                addMultipleChoiceAnswer();
+                fillSwitchesArray();
 
                 break;
             case R.id.remove_answer_butt:
                 //remove answer
-                if (addAnswerLayout.getChildCount() != 0 && createQuestionLayout.getChildAt(0) != null) {
-                    if (createQuestionLayout.getChildAt(createQuestionLayout.getChildCount()-1) instanceof EditText) {
-                        createQuestionLayout.removeViewAt(createQuestionLayout.getChildCount()-1);
-                    }
-                }
+
+                removeMultipleChoiceAnswer();
+                fillSwitchesArray();
 
                 break;
 
@@ -177,6 +218,8 @@ public class CreateQuizFragment extends Fragment implements View.OnClickListener
 
 
     }
+
+    //methods
 
 
     @RequiresApi(api = VERSION_CODES.JELLY_BEAN_MR1)
@@ -195,7 +238,7 @@ public class CreateQuizFragment extends Fragment implements View.OnClickListener
         correct = view.findViewById(R.id.correct_image);
         inCorrect = view.findViewById(R.id.incorrect_image);
         typeSpinner = view.findViewById(R.id.spinner_quest_type);
-        createQuestionLayout = view.findViewById(R.id.inner_create_layout);
+        createQuestionLayout = view.findViewById(R.id.create_question_layout);
 
 
 
@@ -217,41 +260,34 @@ public class CreateQuizFragment extends Fragment implements View.OnClickListener
         switch (selection) {
             case "One correct Answer":
 
-            case "Multiple correct answers":
-                if (createQuestionLayout != null) {
-                    createQuestionLayout.removeAllViews();
-                }
-                inflater = LayoutInflater.from(getContext());
-                addAnswerLayout = (ViewGroup) inflater.inflate(R.layout.add_answer_layout, createQuestionLayout, false);
-                createQuestionLayout.addView(addAnswerLayout);
-                answerButtons = addAnswerLayout.findViewById(R.id.answer_buttons);
-                addAnswerButt = answerButtons.findViewById(R.id.add_answer_butt);
-                removeAnswerButt = answerButtons.findViewById(R.id.remove_answer_butt);
-                if (answerButtons != null)
-                answerButtons.setVisibility(View.VISIBLE);
-                if (addAnswerButt !=null)
-                addAnswerButt.setOnClickListener(this);
-                if (removeAnswerButt !=null)
-                removeAnswerButt.setOnClickListener(this);
-                EditText ans = (EditText) addAnswerLayout.getChildAt(1);
+                createMultipleChoiceQuestion();
+                Switch[] isCorrect = new Switch[10];
 
-                ans.setHint("Insert answer 1");
+                //collect switches into array
+
+                for (int i=1; i<createQuestionLayout.getChildCount(); i++) {
+                    ViewGroup vg = (ViewGroup) createQuestionLayout.getChildAt(i);
+                    Switch sw = (Switch) vg.getChildAt(1);
+                    sw.setOnCheckedChangeListener(this::onCheckedChanged);
+                    isCorrect[i] = sw;
+
+                }
+
+
+
+
+
+                //TODO set the isCorrect switches to be selected only once or give an error dialogue when multiple selected on saveQuestion button press
+
+            case "Multiple correct answers":
+
+                createMultipleChoiceQuestion();
 
 
                 break;
             case "Text answer":
-                if (createQuestionLayout !=null) {
-                    createQuestionLayout.removeAllViews();
-                }
-                inflater = LayoutInflater.from(getContext());
-                addAnswerLayout = (ViewGroup) inflater.inflate(R.layout.add_answer_layout, createQuestionLayout, false);
-                createQuestionLayout.addView(addAnswerLayout);
-                answerButtons = addAnswerLayout.findViewById(R.id.answer_buttons);
-                if (answerButtons != null)
-                    answerButtons.setVisibility(View.GONE);
 
-                ans = (EditText) addAnswerLayout.getChildAt(1);
-                ans.setHint("Insert text answer");
+                createTextQuestion();
 
                 break;
 
@@ -279,4 +315,197 @@ public class CreateQuizFragment extends Fragment implements View.OnClickListener
         }
         return type;
     }
+
+    public void createMultipleChoiceQuestion(){
+
+        if (createQuestionLayout != null) {
+            createQuestionLayout.removeAllViews();
+        }
+        inflater = LayoutInflater.from(getContext());
+        addAnswerLayout = (ViewGroup) inflater.inflate(R.layout.add_answer_layout, createQuestionLayout, false);
+        createQuestionLayout.addView(addAnswerLayout);
+        answerButtons = addAnswerLayout.findViewById(R.id.answer_buttons);
+        addAnswerButt = answerButtons.findViewById(R.id.add_answer_butt);
+        removeAnswerButt = answerButtons.findViewById(R.id.remove_answer_butt);
+        if (answerButtons != null)
+            answerButtons.setVisibility(View.VISIBLE);
+        if (addAnswerButt !=null)
+            addAnswerButt.setOnClickListener(this);
+        if (removeAnswerButt !=null)
+            removeAnswerButt.setOnClickListener(this);
+
+        ViewGroup controlTitles = (ViewGroup) addAnswerLayout.getChildAt(1);
+        TextView isCorrectTv = (TextView) controlTitles.getChildAt(1);
+        isCorrectTv.setVisibility(View.VISIBLE);
+
+    }
+
+    public void createTextQuestion() {
+        if (createQuestionLayout !=null) {
+            createQuestionLayout.removeAllViews();
+        }
+        inflater = LayoutInflater.from(getContext());
+        addAnswerLayout = (ViewGroup) inflater.inflate(R.layout.add_answer_layout, createQuestionLayout, false);
+        createQuestionLayout.addView(addAnswerLayout);
+        answerButtons = addAnswerLayout.findViewById(R.id.answer_buttons);
+        if (answerButtons != null)
+            answerButtons.setVisibility(View.GONE);
+
+        EditText ans = (EditText) inflater.inflate(R.layout.text_answer_et, addAnswerLayout, false);
+        addAnswerLayout.addView(ans);
+
+        controlTitles = (ViewGroup) addAnswerLayout.getChildAt(1);
+        isCorrectTv = (TextView) controlTitles.getChildAt(1);
+
+        ans.setHint("Insert text answer here");
+        isCorrectTv.setVisibility(View.INVISIBLE);
+
+    }
+
+ /*
+    Dialogs
+  */
+
+    public void setQuizNameDialog() {
+
+
+        AlertDialog.Builder quizNameAlertDialog;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            quizNameAlertDialog = new AlertDialog.Builder(getContext(), android.R.style.Theme_Material_Light_Dialog);
+        } else {
+            quizNameAlertDialog = new AlertDialog.Builder(getContext());
+        }
+
+        LayoutInflater inflater1 = LayoutInflater.from(getContext());
+        ViewGroup dialogQuizNameView = (ViewGroup) inflater1.inflate(R.layout.dialog_set_quiz_name, null);
+
+        EditText et = dialogQuizNameView.findViewById(R.id.dialog_qname_et);
+        TextView tv = dialogQuizNameView.findViewById(R.id.dialog_qname_error_tv);
+
+        quizNameAlertDialog.setTitle("Set quiz name")
+                .setView(dialogQuizNameView)
+                .setTitle("Set Quiz Name")
+                .setCancelable(false)
+                .setPositiveButton("Save", null);
+
+
+        final AlertDialog d = quizNameAlertDialog.create();
+        d.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
+
+                Button saveButt = d.getButton(AlertDialog.BUTTON_POSITIVE);
+                saveButt.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+
+                        EditText et = ((AlertDialog) dialog).findViewById(R.id.dialog_qname_et);
+                        TextView tv = ((AlertDialog) dialog).findViewById(R.id.dialog_qname_error_tv);
+
+                        if (et.getText().toString() != null && et.getText().toString().length() > 0) {
+                            quizName = et.getText().toString();
+//                           getActivity().getActionBar().setTitle(quizName);
+                            Toast.makeText(getContext(), "Created quiz: " + quizName, Toast.LENGTH_SHORT).show();
+                            dialog.dismiss();
+                        } else {
+//                            tv.setText("Please enter quiz name");
+                            et.requestFocus();
+                            et.setError("Please enter quiz name");
+
+                        }
+
+                    }
+
+                });
+
+            }
+        });
+
+        d.show();
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            //check if multiple answers are marked as correct
+            int count=0;
+
+                for (int i = 1; i < isCorrect.length; i++) {
+                    Log.d("SwitchArrayList: ", Arrays.toString(isCorrect));
+                    if (isCorrect[i].isChecked()) {
+                        count++;
+
+                    }
+                }
+
+                if (count > 1) {
+
+                        buttonView.setChecked(false);
+                        onlyOneCorrectAllowedDialog();
+                        //buttonView.requestFocus();
+                    //buttonView.setError("Only one correct answer is allowed.");
+                }
+
+        }
+
+
+    public void onlyOneCorrectAllowedDialog() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder = new AlertDialog.Builder(getContext(), android.R.style.Theme_Material_Light_Dialog);
+        } else {
+            builder = new AlertDialog.Builder(getContext());
+        }
+        builder.setTitle(R.string.one_correct_answer_title_dialog)
+                .setMessage(R.string.one_correct_msg_dialog);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
+
+    public void addMultipleChoiceAnswer() {
+        ViewGroup insertAnswer = (ViewGroup) inflater.inflate(R.layout.answer_et, createQuestionLayout, false);
+        EditText answerText = (EditText) insertAnswer.getChildAt(0);
+
+
+        if (VERSION.SDK_INT >= VERSION_CODES.JELLY_BEAN_MR1) {
+            insertAnswer.setId(View.generateViewId());
+        } //else generate id from resourse file ids.xml
+
+        RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams) insertAnswer.getLayoutParams();
+        if (createQuestionLayout.getChildCount() > 0) {
+
+            //if this is the first answer
+            if (addAnswerLayout == createQuestionLayout.getChildAt(0) && insertAnswer == createQuestionLayout.getChildAt(createQuestionLayout.getChildCount()-1)) {
+                lp.addRule(RelativeLayout.BELOW, insertAnswer.getId());
+            } else
+                //if this is not the first answer inserted, reference last item id for layout.below rule
+                lp.addRule(RelativeLayout.BELOW, createQuestionLayout.getChildAt(createQuestionLayout.getChildCount()-1).getId());
+        }
+        insertAnswer.setLayoutParams(lp);
+        createQuestionLayout.addView(insertAnswer);
+        answerText.setHint("Insert answer " + (createQuestionLayout.indexOfChild(createQuestionLayout.getChildAt(createQuestionLayout.getChildCount()-2))+1));
+
+    }
+
+    public void fillSwitchesArray() {
+        isCorrect = new Switch[createQuestionLayout.getChildCount()];
+        for (int i=1; i<createQuestionLayout.getChildCount(); i++) {
+            ViewGroup viewGroup = (ViewGroup) createQuestionLayout.getChildAt(1);
+            isCorrect[i] = (Switch) viewGroup.getChildAt(1);
+            isCorrect[i].setOnCheckedChangeListener(this::onCheckedChanged);
+        }
+    }
+
+    public void removeMultipleChoiceAnswer() {
+        if (createQuestionLayout.getChildAt(1) != null) {
+//                    if (createQuestionLayout.getChildAt(createQuestionLayout.getChildCount()-1) instanceof EditText) {
+            createQuestionLayout.removeViewAt(createQuestionLayout.getChildCount() - 1);
+
+        }
+    }
+
 }
